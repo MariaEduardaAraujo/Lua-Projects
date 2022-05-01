b9e12be8429915c6862e510966e6892bbc5252a6
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-05-01 02:50:17 -0300

    Add files via upload

---
+++ source/funMatematica.lua
@@ -0,0 +1,19 @@
+local matematica = {}
+
+matematica.add = function(x, y)
+	return x + y
+end
+
+matematica.sub = function(x, y)
+	return x - y
+end
+
+matematica.mul = function(x, y)
+	return x * y
+end
+
+matematica.div = function(x, y)
+	return x / y
+end
+
+return matematica
---
+++ source/funMatematica2.lua
@@ -0,0 +1,6 @@
+MathModulo = require("source.matematica")
+
+print(MathModulo.add(12, 1))
+print(MathModulo.sub(12, 1))
+print(MathModulo.mul(12, 1))
+print(MathModulo.div(12, 1))
