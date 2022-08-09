1aa67ae8599d432535153a31dc632648052a8021
Maria Eduarda de Araújo Silva <56840835+MariaEduardaAraujo@users.noreply.github.com>
2022-08-09 17:40:55 -0300

    Update setmetatable.lua

--- archives/algoritmo/setmetatable.lua
+++ archives/algoritmo/setmetatable.lua
@@ -13,3 +13,44 @@ B = {
 
 C = setmetatable(A, {__index = B})
 print(C.b)
+
+Ex2:
+Character = {}
+
+function Character:Andar()
+  print("anda")
+end
+function Character:Correr()
+  print("corre")
+end
+function Character:Pular()
+  print("pula")
+end
+
+-----------------------------------
+
+CharacterSolder = {}
+
+function CharacterSolder:Atacar()
+  print("Ataca")
+end
+
+CharacterSolder = setmetatable(CharacterSolder, {__index = Character})
+
+CharacterSolder:Atacar()
+
+-----------------------------------
+
+Enemy = {}
+
+function Enemy:PerseguirJogador()
+  print("persegue")
+end
+
+-----------------------------------
+
+Player = {}
+
+function Player:SerControlado()
+  print("sendo controlado")
+end
